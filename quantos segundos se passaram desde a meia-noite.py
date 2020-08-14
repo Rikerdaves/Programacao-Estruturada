dd9158f4eabebfa27d89dc48b3821d824da39efd
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-14 14:27:46 -0300

    Update quantos segundos se passaram desde a meia-noite.py

--- quantos segundos se passaram desde a meia-noite.py
+++ quantos segundos se passaram desde a meia-noite.py
@@ -2,14 +2,11 @@ a = int(input("Digite a Hora: "))
 b = int(input("Digite os minutos: "))
 c = int(input("Digite os segundos: "))
 
-h = 24 - a
-m = 60 - b
-s = 60 - c
+h = a * 3600 
+m = b * 60
+s = c
 
-#transformando hora e minuto em segundos
-h2 = h * 3600
-m2 = m * 60
 
-resultado = h2 + m2 + s
+resultado = h + m + s
 
-print(f"Se passaram {resultado} segundos desde a hora inserida {a}:{b}:{c}")
+print(f"Se passaram {resultado} segundos desde a meia-noite")
