da55c7d94232291ffcb7771dddc20bc21e281e36
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-16 21:37:12 -0300

    Update quantos segundos se passaram desde a meia-noite.py

--- quantos segundos se passaram desde a meia-noite.py
+++ quantos segundos se passaram desde a meia-noite.py
@@ -1,12 +1,18 @@
-a = int(input("Digite a Hora: "))
-b = int(input("Digite os minutos: "))
-c = int(input("Digite os segundos: "))
+def quest2_d():
+  return
 
-h = a * 3600 
-m = b * 60
-s = c
+def main():
+  a = int(input("Digite a Hora: "))
+  b = int(input("Digite os minutos: "))
+  c = int(input("Digite os segundos: "))
 
+  h = a * 3600 
+  m = b * 60
+  s = c
+ 
+  resultado = h + m + s
+  
+  print(f"Se passaram {resultado} segundos desde a meia-noite")
 
-resultado = h + m + s
-
-print(f"Se passaram {resultado} segundos desde a meia-noite")
+if __name__ == '__main__':
+  main()
