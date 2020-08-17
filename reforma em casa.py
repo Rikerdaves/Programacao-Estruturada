1663b74517c26f6806f64f6b3ccc821f8898c0a8
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-16 21:40:02 -0300

    Update reforma em casa.py

--- reforma em casa.py
+++ reforma em casa.py
@@ -1,14 +1,22 @@
-altura = int(input("Digite a Altura: "))
-comprimento = int(input("Digite o comprimento: "))
-largura = int(input("Digite a largura: "))
+def quest2_d():
+  return
 
-area_piso_sala = altura * largura
+def main():
+    altura = int(input("Digite a Altura: "))
+    comprimento = int(input("Digite o comprimento: "))
+    largura = int(input("Digite a largura: "))
 
-volume_sala = largura * comprimento * altura
+    area_piso_sala = altura * largura
 
-area_paredes = 2 * altura * largura + 2 * altura * comprimento 
+    volume_sala = largura * comprimento * altura
+
+    area_paredes = 2 * altura * largura + 2 * altura * comprimento 
 
 
-print(f'Área do piso da sala: {area_piso_sala} metros quadrados\n'
-    f'Volume da sala: {volume_sala} metros cubicos\n'
-    f'Área das paredes da sala: {area_paredes} metros quadrados')
+    print(f'Área do piso da sala: {area_piso_sala} metros quadrados\n'
+          f'Volume da sala: {volume_sala} metros cubicos\n'
+          f'Área das paredes da sala: {area_paredes} metros quadrados')
+
+if __name__ == '__main__':
+  main()    
+
