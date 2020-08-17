0bda8d9007750cad58ff088c08f41f1d3eb9b525
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-16 21:33:03 -0300

    Update Media.py

--- Media.py
+++ Media.py
@@ -1,7 +1,18 @@
-a = int(input("Digite o primeiro número: "))
-b = int(input("Digite o segundo número: "))
-c = int(input("Digite o terceiro número: "))
+  
+def quest2_b():
+  return
+  
+def main(): 
+  a = int(input("Digite o primeiro número: "))
+  b = int(input("Digite o segundo número: "))
+  c = int(input("Digite o terceiro número: "))
 
-media = (a + b + c)/3
+  media = (a + b + c)/3
 
-print(f'A média dos números é {media}')
+  print(f'A média dos números é {media}')
+
+ 
+  
+  
+if __name__ == '__main__':
+  main()
