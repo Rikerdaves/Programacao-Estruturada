ebc984547ceab5d7857173fe6480491e275c18f6
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-16 21:31:41 -0300

    Add files via upload

---
+++ Cubo de um numero.py
@@ -0,0 +1,11 @@
+  
+def quest2_c():
+  return
+  
+def main():  
+  a = int(input('Digite um Número: '))
+  cubo = a**3
+  print(f'O número elevado ao cubo é {cubo}')
+  
+if __name__ == '__main__':
+  main()
