ca2501f41e22f0d98ad3f32c84b3d709caeeb897
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-30 16:38:03 -0300

    Add files via upload

---
+++ Sem-03-T2-Q1.py
@@ -0,0 +1,30 @@
+def vogal(z):
+    v = True
+    if z == "a":
+        return v
+    if z == "e":
+        return v
+    if z == "i":
+        return v
+    if z == "o":
+        return v
+    if z == "u":
+        return v
+    if z == "A":
+        return v
+    if z == "E":
+        return v
+    if z == "I":
+        return v
+    if z == "O":
+        return v
+    if z == "U":
+        return v
+    else:
+        return not v
+def main():
+    z = str(input("Digite uma vogal: "))
+    print (f'{vogal(z)}')
+
+if __name__ == "__main__":
+    main()
