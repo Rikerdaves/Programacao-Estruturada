02ece0e5a78b7ebc2447448c196bb77d18782392
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-31 00:36:57 -0300

    Update Sem-03-T2-Q5.py

--- Sem-03-T2-Q5.py
+++ Sem-03-T2-Q5.py
@@ -1,16 +1,21 @@
-def vogal_ou_consoante(letra):
-    if letra in 'a e i o u':
+def alfabeto_numeros(letra):
+    vogal = 'a e i o u'
+    consoante = 'b c d f g h j k l m n p q r s t v x z y w' 
+    if letra.lower() in vogal :
         return f'vogal'
-    if letra in 'b c d f g h j k l m n p q r s t v w x y z':
-        return f'consoante'
-    elif float(letra):
-        return  f'numero'
+    elif letra.lower() in consoante:
+        return f'consoante'  
+    elif letra.isdigit():
+        return f'número'
     else:
-        return f'simbolo'      
+        return f'símbolo' 
+    
+def  main():
+    letra = input('Digite um caractere: ')
 
-def main():
-    letra = str(input('Digite um caractere: '))
-    print(f'{vogal_ou_consoante(letra)}')
+    executar = alfabeto_numeros(letra)
 
-if __name__ == "__main__":
-    main()            
+    print(executar)
+
+if __name__== "__main__":
+    main()
