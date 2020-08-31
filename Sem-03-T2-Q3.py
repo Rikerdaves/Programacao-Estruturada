2e3a58d145932a2d49559809c8b4430a0f81115f
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-30 23:28:56 -0300

    Update Sem-03-T2-Q3.py

--- Sem-03-T2-Q3.py
+++ Sem-03-T2-Q3.py
@@ -1,5 +1,5 @@
 def consoante(letra):
-    if letra in ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'):
+    if letra.lower() in 'b c d f g h j k l m n p q r s t v w x y z':
         return True
     else:
         return False
