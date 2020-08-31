8af51a6a433bd522ce90ca2aa20bf570fdf878bc
Rikerdaves <63235580+Rikerdaves@users.noreply.github.com>
2020-08-30 23:23:28 -0300

    Update Sem-03-T2-Q2.py

--- Sem-03-T2-Q2.py
+++ Sem-03-T2-Q2.py
@@ -1,14 +1,12 @@
 def vogal_ou_consoante(letra):
-    if letra in ('a', 'e', 'i', 'o', 'u'):
+    if letra.lower() in 'a e i o u b c d f g h j k l m n p q r s t v x z w y':
         return True
-    elif letra in ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'):
-        return False
     else:
-        raise ValueError('Não é vogal nem consoante')
+        return False
 
 def main():
-    letra = str(input('Digite uma lera: '))
+    letra = str(input('Digite uma letra: '))
     print(f'{vogal_ou_consoante(letra)}')
 
 if __name__ == "__main__":
-    main()        
+    main()      
